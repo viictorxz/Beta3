5b4b6a9e29b523c2bee8c9a018558abbc5913664
ExTRUPÍCIO <61525197+viictorxz@users.noreply.github.com>
2021-01-30 00:33:30 -0300

    Add files via upload

---
+++ src/magagumenu.js
@@ -0,0 +1,8 @@
+const magagumenu = (prefix, pushname) => {
+    return `◪ *Comandos do Magagu*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
---
+++ src/menuadmin.js
@@ -0,0 +1,33 @@
+const menuadmin = (prefix, pushname) => {
+ return `Paradas dos ADM aki menó
+ _Comandos Limitados apenas pros ADM, esse povo feio da desgraça_
+ 
+ ◪ *COMANDO DOS ADMINS*
+ │
+ ├─ ❏ ${prefix}opengc
+ ├─ ❏ ${prefix}closegc
+ ├─ ❏ ${prefix}promote
+ ├─ ❏ ${prefix}demote
+ ├─ ❏ ${prefix}tagall
+ ├─ ❏ ${prefix}tagall2
+ ├─ ❏ ${prefix}tagall3
+ ├─ ❏ ${prefix}tagall4
+ ├─ ❏ ${prefix}tagall5
+ ├─ ❏ ${prefix}add
+ ├─ ❏ ${prefix}kick
+ ├─ ❏ ${prefix}listadmins
+ ├─ ❏ ${prefix}linkgroup
+ ├─ ❏ ${prefix}leave
+ ├─ ❏ ${prefix}welcome
+ ├─ ❏ ${prefix}nsfw
+ ├─ ❏ ${prefix}leveling
+ ├─ ❏ ${prefix}level
+ ├─ ❏ ${prefix}delete
+ ├─ ❏ ${prefix}simih
+ └─ ❏ ${prefix}ownergroup
+ `
+
+
+}
+
+exports.menuadmin = menuadmin

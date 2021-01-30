e791c802b0cdd41eec97cf6928737b7d34544aba
ExTRUPÍCIO <61525197+viictorxz@users.noreply.github.com>
2021-01-30 00:36:38 -0300

    Add files via upload

---
+++ database/menu/owner.js
@@ -0,0 +1,23 @@
+const owner = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerBot}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *OWNER*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall`
+}
+exports.owner = owner
